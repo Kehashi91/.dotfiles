9bad557d6a2d618221460d81f5e852b6647e71ad
Kehashi91 <25392586+Kehashi91@users.noreply.github.com>
2025-11-19 16:18:57 +0000

    docker sources fixes

--- scripts/install_apps.sh
+++ scripts/install_apps.sh
@@ -79,9 +79,7 @@ sudo apt install ca-certificates curl
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
-
-# Add the repository to Apt sources:
-sudo tee /etc/apt/sources.list.d/docker.sources <<EOF
+sudo cp misc/docker.source /etc/apt/sources.list.d
 
 sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
